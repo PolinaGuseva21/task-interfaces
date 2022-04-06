bdaf0c6ec345cad479abfd7f9224a5459b315b03
PolinaGuseva21 <pn_guseva@mail.ru>
2022-04-06 18:07:20 +0300

    task-interfaces

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -11,7 +11,7 @@ export function getPerson(): IPersonData {
     return {
         name: 'Petr',
         secondName: 'Smith',
-        age: '7',
+        age: 7,
     };
 }
 
@@ -27,6 +27,23 @@ export interface IStudentData {
 Данные должны храниться в объектах, соответствующих интерфейсу `IStudentData`.
  */
 export function getStudentsData(): IStudentData[] {
+    let array: IStudentData[] = [];
+    let person1: IStudentData = {
+        name: 'Ivan',
+        secondName: 'Petrov',
+        age: 20,
+        phone: '+7(555)555-55-50',
+    };
+    let person2: IStudentData = {
+        name: 'Stepan',
+        secondName: 'Petrov',
+        age: 19,
+        phone: '+7(555)555-55-51',
+    };
+    let person3: IStudentData = { name: 'Petr', secondName: 'Ivanov', age: 19 };
+    array.push(person1, person2, person3);
+    return array;
+
     /**
      * Данные студентов которые нужно передать:
      *
